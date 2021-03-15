d79bc55ae4fd591d162c3bd47bfe75328ce09178
ndaemy <yuyaebean@gmail.com>
2021-03-15 16:52:28 +0900

    1초에 1번씩 숫자 찍기

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import { Observable } from "rxjs";
+import { from, interval, of, zip } from 'rxjs';
+import { concatMap, delay, map, mergeMap, take } from 'rxjs/operators';
 
-const observable = new Observable((subscriber) => {
-  subscriber.next(1);
-  subscriber.next(2);
-  subscriber.next(3);
-  setTimeout(() => {
-    subscriber.next(4);
-    subscriber.complete();
-  }, 1000);
-});
+const list = [1, 2, 3, 4, 5];
 
-console.log("just before subscribe");
-observable.subscribe({
-  next(x) {
-    console.log("got value " + x);
-  },
-  error(err) {
-    console.error("something wrong occurred: " + err);
-  },
-  complete() {
-    console.log("done");
-  },
-});
-console.log("just after subscribe");
+// from(list)
+//   .pipe(
+//     mergeMap(i => of(i).pipe(delay(i * 1000))),
+//     take(10)
+//   )
+//   .subscribe(console.log);
+
+// from(list)
+//   .pipe(mergeMap(i => of(i).pipe(delay(i * 1000))))
+//   .subscribe(console.log);
+
+// from(list)
+//   .pipe(concatMap(val => of(val).pipe(delay(1000))))
+//   .subscribe(console.log);
+
+interval(1000)
+  .pipe(
+    take(list.length),
+    map(i => list[i])
+  )
+  .subscribe(console.log);
